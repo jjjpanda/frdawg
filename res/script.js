924a4be8996dc2049a31772b9c4f49a1b80b1185
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2022-09-16 15:37:26 -0400

    updates

--- res/script.js
+++ res/script.js
@@ -1,12 +1,33 @@
+const scrollToTop = function () {
+    window.scrollTo(0, 0);
+};
+
+window.onbeforeunload = scrollToTop
+window.onbeforeprint = scrollToTop
+
 const randomFromList = (l) => l[Math.floor(Math.random() * list.length)]
 
 const imageFromDrive = randomFromList(list);
+var src = document.getElementById("imgbox");
 
 var img = document.createElement("img");
-img.src = `https://drive.google.com/uc?export=view&id=${imageFromDrive.id}`;
+img.src = `https://drive.google.com/uc?export=view&id=2${imageFromDrive.id}`;
 img.className = "center-fit"
+
+const onError = () => {
+    var div = document.createElement("div");
+    div.textContent = "🤨";
+    div.style.fontSize = "20vh"
+
+    var explanation = document.createElement("p");
+    explanation.textContent = "there should be a random image here but something went wrong, i blame google drive api"
+
+    src.removeChild(img)
+    src.appendChild(div)
+    src.appendChild(explanation)
+}
+
+img.onerror = onError;
 console.log(img)
 
-var src = document.getElementById("imgbox");
-console.log(src)
 src.appendChild(img);
